8e87b4aa1d73ba102395a031329b0d0237a8cf17
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-15 13:38:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App() {
 
       <Routes>
         <Route path="/LoginSystem_Mern/Register" element={<Register />} />
-        <Route path="/Success" element={<Success />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/LoginSystem_Mern/Success" element={<Success />} />
+        <Route path="/LoginSystem_Mern/Home" element={<Home />} />
 
         <Route path="/LoginSystem_Mern/Forgot" element={<Forgot />} />
 
