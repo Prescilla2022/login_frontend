d4d18551cdd11a9331a18f072e5e2e8b1ace6699
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-16 09:04:08 +0200

    Update UpdatedStatus.js

--- src/components/UpdatedStatus.js
+++ src/components/UpdatedStatus.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function UpdatedStatus() {
   return (
     <div className="status">
-      <p>Password Updated successfully</p>
+      <p>Password updated successfully</p>
       <p>Click here to</p>
       <Link to="/">Log In</Link>
     </div>
