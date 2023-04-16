8ebafc8be59854e4b8443a719653ffd3ef684c4c
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-16 09:02:42 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="status">
       <p>Welcome </p>
-      <p>You are successfully loggedIn</p>
+      <p>You are successfully logged in</p>
     </div>
   );
 }
